5ab48424ffaaebd1621c79293abe07e1be60e889
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-08-05 15:12:32 -0300

    Create CRUD Carts

--- src/controller/CartsController.js
+++ src/controller/CartsController.js
@@ -1,6 +1,71 @@
+import Cart from '../models/carts'
+
 class CartsController {
   async index(req, res) {
-    return res.status(200).json({ foo: 'bar' })
+    try {
+      const carts = await Cart.find()
+      return res.status(200).json(carts)
+    }
+    catch (err) {
+      console.error(err)
+      return res.status(500).json({ error: 'Internal Server Error' })
+    }
+  }
+
+
+  async create(req, res) {
+    try {
+      const { code, price } = req.body
+
+      const cart = await Cart.create({
+        code, price
+      })
+
+      return res.status(201).json(cart)
+    }
+
+    catch (err) {
+      console.error(err)
+      return res.status(500).json({ error: 'Internal Server Error' })
+    }
+  }
+
+
+  async update(req, res) {
+    try {
+      const { id } = req.params
+      const { code, price } = req.body
+
+      const cart = await Cart.findById(id)
+
+      if (!cart) {
+        return res.status(404).json({ error: 'not found' })
+      }
+
+      await cart.updateOne({ code, price })
+
+      return res.status(200).json()
+    }
+    catch (err) {
+      console.error(err)
+      return res.status(500).json({ error: 'Internal Server Error' })
+    }
+  }
+
+  async destroy(req, res) {
+    try {
+      const { id } = req.params
+      const cart = Cart.findById(id)
+      if (!cart) {
+        return res.status(404).json({ error: 'not found' })
+      }
+
+      await cart.deleteOne()
+
+    } catch (error) {
+      console.error(err)
+      return res.status(500).json({ error: 'Internal Server Error' })
+    }
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ import CartsController from "./controller/CartsController"
 
 const routes = new Router()
 routes.get("/carts", CartsController.index)
+routes.post("/carts", CartsController.create)
+routes.put('/carts/:id', CartsController.update)
+routes.delete('/carts/:id', CartsController.destroy)
 
 export default routes
