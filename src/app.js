eafbf9f83c453120586096b1bc1e51a15b5bb368
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-08-05 11:58:05 -0300

    Criando estrutura do server

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+
+class App {
+  constructor() {
+    this.server = express()
+    this.middlewares()
+    this.routes()
+  }
+  middlewares() {
+    this.server.use(express.json())
+    this.server.use(cors())
+  }
+
+  routes() {
+
+  }
+}
+
+export default new App.server()
